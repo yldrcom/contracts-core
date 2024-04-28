b54a7a7934ebcdaab224d637f7f2cf264fdfedae
Arsenii Kulikov <klkvrr@gmail.com>
2024-04-28 05:28:12 +0400

    use OZ lib

--- src/protocol/concentrated-liquidity/CLAdapterWrapper.sol
+++ src/protocol/concentrated-liquidity/CLAdapterWrapper.sol
@@ -2,16 +2,19 @@
 pragma solidity ^0.8.10;
 
 import {BaseCLAdapter} from "./adapters/BaseCLAdapter.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 
 /// @notice A library to delegate calls to a BaseCLAdapter contract
 library CLAdapterWrapper {
+    using Address for address;
+
     function delegateIncreaseLiquidity(BaseCLAdapter adapter, uint256 tokenId, uint256 amount0, uint256 amount1)
         internal
         returns (uint256 amount0Resulted, uint256 amount1Resulted)
     {
-        (bool success, bytes memory result) =
-            address(adapter).delegatecall(abi.encodeCall(adapter.increaseLiquidity, (tokenId, amount0, amount1)));
-        require(success);
+        bytes memory result = address(adapter).functionDelegateCall(
+            abi.encodeCall(adapter.increaseLiquidity, (tokenId, amount0, amount1))
+        );
         return abi.decode(result, (uint256, uint256));
     }
 
@@ -19,9 +22,8 @@ library CLAdapterWrapper {
         internal
         returns (uint256 amount0Resulted, uint256 amount1Resulted)
     {
-        (bool success, bytes memory result) =
-            address(adapter).delegatecall(abi.encodeCall(adapter.decreaseLiquidity, (tokenId, liquidity)));
-        require(success);
+        bytes memory result =
+            address(adapter).functionDelegateCall(abi.encodeCall(adapter.decreaseLiquidity, (tokenId, liquidity)));
         return abi.decode(result, (uint256, uint256));
     }
 
@@ -32,10 +34,9 @@ library CLAdapterWrapper {
         uint128 amount1Max,
         address recipient
     ) internal returns (uint256 amount0Resulted, uint256 amount1Resulted) {
-        (bool success, bytes memory result) = address(adapter).delegatecall(
+        bytes memory result = address(adapter).functionDelegateCall(
             abi.encodeCall(adapter.collectFees, (tokenId, amount0Max, amount1Max, recipient))
         );
-        require(success);
         return abi.decode(result, (uint256, uint256));
     }
 
@@ -43,9 +44,7 @@ library CLAdapterWrapper {
         internal
         returns (uint256 tokenId, uint128 liquidity, uint256 amount0, uint256 amount1)
     {
-        (bool success, bytes memory result) =
-            address(adapter).delegatecall(abi.encodeCall(adapter.mintPosition, (params)));
-        require(success);
+        bytes memory result = address(adapter).functionDelegateCall(abi.encodeCall(adapter.mintPosition, (params)));
         return abi.decode(result, (uint256, uint128, uint256, uint256));
     }
 }
